2d3a1482653e7733116588e0fa49e16d9a9a3371
Damith C. Rajapakse <damith@gmail.com>
2023-05-02 22:12:54 +0800

    Increase end date

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --master
-java -jar RepoSense.jar --config ./configs --until 24/04/2023 --since 09/01/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./configs --until 02/05/2023 --since 09/01/2023  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
